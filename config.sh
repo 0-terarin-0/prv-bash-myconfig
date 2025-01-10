41396303a66c97a005775ba20e8d5d08667507c6
0-terarin-0 <terarin@terarin-iw.net>
2025-01-10 16:54:52 +0900

    [edit]Changed config.sh [edit]Updated README.md

--- config.sh
+++ config.sh
@@ -29,7 +29,7 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 )
 
 # Install zsh-autosuggestion
-echo "Install zsh-autosuggestion"
+echo "Install zsh-autosuggestions"
 (
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 )
